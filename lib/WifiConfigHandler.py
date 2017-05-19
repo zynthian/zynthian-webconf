d76cdd842fae04e7dd2c4a4e2da9bfb5cd0381af
Markus Heidt <markus.heidt.sp@lhsystems.com>
2017-05-20 01:44:33 +0200

    webconf-WIFI: Veil password, save new password and save priority

--- lib/WifiConfigHandler.py
+++ lib/WifiConfigHandler.py
@@ -9,6 +9,9 @@ from collections import OrderedDict
 #------------------------------------------------------------------------------
 
 class WifiConfigHandler(tornado.web.RequestHandler):
+
+	fieldMap = {"ZYNTHIAN_WIFI_PRIORITY": "priority"}
+
 	def prepare(self):
 		self.genjson=False
 		try:
@@ -20,7 +23,9 @@ class WifiConfigHandler(tornado.web.RequestHandler):
 
 	def get(self, errors=None):
 		supplicant_file_name = self.getSupplicantFileName()
-		supplicant_data = self.readSupplicantData(supplicant_file_name)
+		supplicant_data = re.sub(r'psk=".*?"', 'psk="*****"',
+			self.readSupplicantData(supplicant_file_name),
+			re.IGNORECASE | re.MULTILINE | re.DOTALL )
 		p = re.compile('.*?network=\\{.*?ssid=\\"(.*?)\\".*?psk=\\"(.*?)\\".*?priority=(\d*).*?\\}.*?', re.IGNORECASE | re.MULTILINE | re.DOTALL )
 		iterator = p.finditer(supplicant_data)
 		config=OrderedDict([
@@ -44,8 +49,6 @@ class WifiConfigHandler(tornado.web.RequestHandler):
 			})
 			idx+=1
 
-		print(networks)
-
 		config['ZYNTHIAN_WIFI_NETWORKS'] = networks
 
 		if self.genjson:
@@ -55,8 +58,11 @@ class WifiConfigHandler(tornado.web.RequestHandler):
 
 
 	def post(self):
-		fo = open("/etc/wpa_supplicant/wpa_supplicant.conf", "w")
 		wpa_supplicant_data = self.get_argument('ZYNTHIAN_WIFI_WPA_SUPPLICANT')
+		fieldNames = ["ZYNTHIAN_WIFI_PRIORITY"]
+		wpa_supplicant_data = self.applyUpdatedFields(wpa_supplicant_data, fieldNames)
+
+		fo = open("/etc/wpa_supplicant/wpa_supplicant.conf", "w")
 		fo.write(wpa_supplicant_data)
 		fo.flush()
 		fo.close()
@@ -72,4 +78,32 @@ class WifiConfigHandler(tornado.web.RequestHandler):
 		fo = open(supplicant_file_name, "r")
 		wpa_supplicant_data = "".join(fo.readlines())
 		fo.close()
+		return wpa_supplicant_data
+
+	def applyUpdatedFields(self, wpa_supplicant_data, fieldNames):
+		supplicant_file_name = self.getSupplicantFileName()
+		previous_supplicant_data = self.readSupplicantData(supplicant_file_name)
+
+		p = re.compile('.*?network=\\{.*?ssid=\\"(.*?)\\".*?psk=\\"(.*?)\\".*?\\}.*?', re.IGNORECASE | re.MULTILINE | re.DOTALL )
+		iterator = p.finditer(previous_supplicant_data)
+		for m in iterator:
+			print(m.group(1) + '-' + m.group(2))
+			newPassword =  self.get_argument('ZYNTHIAN_WIFI_PSK_' + m.group(1))
+			if not newPassword: newPassword = m.group(2)
+
+			pReplacePasswordVeil = re.compile('ssid=\\"' + m.group(1) + '\\"(.*?psk=)\\".*?\\"', re.IGNORECASE | re.MULTILINE | re.DOTALL )
+			wpa_supplicant_data = pReplacePasswordVeil.sub('ssid="' + m.group(1) + r'"\1"' + newPassword + '"', wpa_supplicant_data)
+
+			for fieldName in fieldNames:
+				fieldUpdated =  self.get_argument(fieldName + '_' + m.group(1) + '_UPDATED')
+				if fieldUpdated:
+					fieldValue =  self.get_argument(fieldName + '_' + m.group(1))
+					print(self.fieldMap[fieldName])
+					print(fieldValue)
+					regexp = 'ssid=\\"' + m.group(1) + '\\"(?P<pre>.*?' + self.fieldMap[fieldName] + '=)\\S+(?P<post>.*\\})'
+					print(regexp)
+					pReplacement = re.compile(regexp, re.IGNORECASE | re.MULTILINE | re.DOTALL )
+					wpa_supplicant_data = pReplacement.sub("ssid=\"" + m.group(1) + "\"" + r'\g<pre>' + str(fieldValue) + r'\g<post> ', wpa_supplicant_data)
+
+
 		return wpa_supplicant_data
