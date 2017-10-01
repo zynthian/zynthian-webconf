a7cb4af6b6d3f699062fddd0fc04386db83472f4
Markus Heidt <markus.heidt.sp@lhsystems.com>
2017-10-01 03:06:26 +0200

    #19 Midi Filter/Translation

--- lib/MidiConfigHandler.py
+++ lib/MidiConfigHandler.py
@@ -23,17 +23,51 @@ class MidiConfigHandler(ZynthianConfigHandler):
 			'ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_DOWN': 'C#00',
 			'ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_UP': 'B#007F',
 			'ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_DOWN': 'B#0000'
-
 		}]
 	])
 
 	program_change_mode_labels=OrderedDict([
-		['32', 'LSB'],
+		['2', 'LSB'],
 		['0', 'MSB']
+	])
+
+	midi_cc_labels=OrderedDict([
+		['1', '1 - Modulation Wheel'],
+		['2', '2 - Breath controller'],
+		['4', '4 - Foot Pedal'],
+		['5', '5 - Portamento Time'],
+		['6', '6 - Data Entry'],
+		['7', '7 - Volume'],
+		['10', '10 - Pan Position']
+	])
+
+	midi_event_options=OrderedDict([
+		['PG', 'Program change'],
+		['KP', 'Polyphonic Key Pressure (Aftertouch)'],
+		['CP', 'Channel Pressure (Aftertouch)'],
+		['PB', 'Pitch Bending'],
+		['CC', 'Continuous Controller Change']
 	])
 
 	@tornado.web.authenticated
 	def get(self, errors=None):
+		add_panel_config=OrderedDict([
+			['FILTER_ADD_MIDI_EVENT', {
+				'options': list(self.midi_event_options.keys()),
+				'option_labels': self.midi_event_options
+			}],
+			['FILTER_ADD_TRANSLATED_MIDI_EVENT', {
+				'options': list(self.midi_event_options.keys()),
+				'option_labels': self.midi_event_options
+			}],
+			['FILTER_ADD_CC_VALUE', {
+				'option_labels': self.midi_cc_labels
+			}],
+			['FILTER_ADD_TRANSLATED_CC_VALUE', {
+				'option_labels': self.midi_cc_labels
+			}]
+		])
+
 		config=OrderedDict([
 			['ZYNTHIAN_PRESET_PRELOAD_NOTEON', {
 				'type': 'boolean',
@@ -90,6 +124,17 @@ class MidiConfigHandler(ZynthianConfigHandler):
 				'title': 'Master Bank change-down',
 				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_DOWN',''),
 				'advanced': True
+			}],
+			['ZYNTHIAN_MASTER_MIDI_EVENT_TRANSLATIONS', {
+				'type': 'textarea',
+				'title': 'Translate midi events',
+				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_EVENT_TRANSLATIONS',""),
+				'cols': 50,
+				'rows': 5,
+				'addButton': 'midi_translation_add',
+				'addPanel': 'midi_translation.html',
+				'addPanelConfig': add_panel_config,
+				'advanced': True
 			}]
 		])
 		if self.genjson:
@@ -98,7 +143,35 @@ class MidiConfigHandler(ZynthianConfigHandler):
 			self.render("config.html", body="config_block.html", config=config, title="MIDI", errors=errors)
 
 	def post(self):
+		escaped_request_arguments = tornado.escape.recursive_unicode(self.request.arguments)
+		if 'FILTER_ADD_COMMAND' in escaped_request_arguments and 'FILTER_ADD_MIDI_EVENT' in escaped_request_arguments:
+			self.calculateTranslation(escaped_request_arguments)
 		self.request.arguments['ZYNTHIAN_PRESET_PRELOAD_NOTEON'] = self.request.arguments.get('ZYNTHIAN_PRESET_PRELOAD_NOTEON','0')
-		errors=self.update_config(tornado.escape.recursive_unicode(self.request.arguments))
+		errors=self.update_config(escaped_request_arguments)
 		self.restart_ui()
 		self.get(errors)
+
+	def calculateTranslation(self, escaped_request_arguments):
+		if escaped_request_arguments['FILTER_ADD_COMMAND'][0]:
+			channels = ''
+			if 'FILTER_ADD_CHANNEL' in escaped_request_arguments and escaped_request_arguments['FILTER_ADD_CHANNEL'][0]:
+				channels =  'CH#' + ','.join(str(x) for x in escaped_request_arguments['FILTER_ADD_CHANNEL'])
+			midiEvent = ''
+			if 'FILTER_ADD_MIDI_EVENT' in escaped_request_arguments and escaped_request_arguments['FILTER_ADD_MIDI_EVENT'][0]:
+				midiEvent = escaped_request_arguments['FILTER_ADD_MIDI_EVENT'][0]
+			ccValue = ''
+			if 'FILTER_ADD_CC_VALUE' in escaped_request_arguments and escaped_request_arguments['FILTER_ADD_CC_VALUE'][0]:
+				ccValue = '#' +  ','.join(str(x) for x in escaped_request_arguments['FILTER_ADD_CC_VALUE'])
+			translatedMidiEvent = ''
+			if 'FILTER_ADD_TRANSLATED_MIDI_EVENT' in escaped_request_arguments and escaped_request_arguments['FILTER_ADD_TRANSLATED_MIDI_EVENT'][0]:
+				translatedMidiEvent = '=> ' + escaped_request_arguments['FILTER_ADD_TRANSLATED_MIDI_EVENT'][0]
+				if 'FILTER_ADD_TRANSLATED_CC_VALUE' in escaped_request_arguments and escaped_request_arguments['FILTER_ADD_TRANSLATED_CC_VALUE'][0]:
+					translatedMidiEvent += '#' +  ','.join(str(x) for x in escaped_request_arguments['FILTER_ADD_TRANSLATED_CC_VALUE'])
+
+			newLine = escaped_request_arguments['FILTER_ADD_COMMAND'][0] + ' ' + channels + ' ' + midiEvent + ' ' + ccValue + ' ' + translatedMidiEvent
+			logging.info("new line : %s" % (newLine))
+			escaped_request_arguments['ZYNTHIAN_MASTER_MIDI_EVENT_TRANSLATIONS'][0] += 	"\n" + newLine
+
+		for filterAddArgument in list(escaped_request_arguments.keys()):
+			if filterAddArgument.startswith('FILTER_ADD'):
+				del escaped_request_arguments[filterAddArgument]
