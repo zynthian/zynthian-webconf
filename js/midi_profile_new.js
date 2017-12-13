785eccd9222f283db46cbfb05d1fe094da8e4b47
Markus Heidt <markus.heidt.sp@lhsystems.com>
2017-12-13 22:46:05 +0100

    Midi profiles as envar scripts

---
+++ js/midi_profile_new.js
@@ -0,0 +1,10 @@
+$('#zynthian_midi_profile_new_script').click(
+	function(){
+		inputName = $('#zynthian_midi_profile_new_script_name');
+		if (!inputName.val()){
+			alert('Please enter a script name!');
+		} else {
+			inputName[0].form.submit();
+		}
+	}
+);
--- lib/midi_config_handler.py
+++ lib/midi_config_handler.py
@@ -26,6 +26,7 @@ import os
 import sys
 import tornado.web
 import logging
+import re
 from collections import OrderedDict
 from subprocess import check_output
 from lib.ZynthianConfigHandler import ZynthianConfigHandler
@@ -38,19 +39,21 @@ from zyngine.zynthian_midi_filter import MidiFilterScript
 #------------------------------------------------------------------------------
 
 class MidiConfigHandler(ZynthianConfigHandler):
+	PROFILE_SYS_DIRECTORY = "/zynthian/zynthian-data/midi-profiles"
+	PROFILE_USER_DIRECTORY = "/zynthian/zynthian-my-data/midi-profiles"
 
 	midi_program_change_presets=OrderedDict([
 		['Custom', {
-			'ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_UP': '',
-			'ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_DOWN': '',
-			'ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_UP': '',
-			'ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_DOWN': ''
+			'ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_UP': '',
+			'ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_DOWN': '',
+			'ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_UP': '',
+			'ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_DOWN': ''
 		}],
 		['Roland', {
-			'ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_UP': 'C#7F',
-			'ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_DOWN': 'C#00',
-			'ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_UP': 'B#007F',
-			'ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_DOWN': 'B#0000'
+			'ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_UP': 'C#7F',
+			'ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_DOWN': 'C#00',
+			'ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_UP': 'B#007F',
+			'ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_DOWN': 'B#0000'
 		}]
 	])
 
@@ -124,8 +127,24 @@ class MidiConfigHandler(ZynthianConfigHandler):
 		['CC', 'Continuous Controller Change']
 	])
 
+	midi_profile_presets=OrderedDict([])
+
+
+	def prepare(self):
+		super(MidiConfigHandler,self).prepare()
+		self.load_midi_profile_directories()
+
+
+
 	@tornado.web.authenticated
 	def get(self, errors=None):
+		self.load_midi_profiles()
+		self.midi_envs = self.midi_profile_presets[self.current_midi_profile_script]
+		ports_config=self.get_ports_config()
+
+
+
+
 		add_panel_config=OrderedDict([
 			['FILTER_ADD_MIDI_EVENT', {
 				'options': list(self.midi_event_options.keys()),
@@ -143,69 +162,104 @@ class MidiConfigHandler(ZynthianConfigHandler):
 			}]
 		])
 
-		ports_config=self.get_ports_config()
 
+
+
+		#upper case ZYNTHIAN_MIDI will be stored in profile file
+		#other upper case in zynthian_envar
 		config=OrderedDict([
-			['ZYNTHIAN_PRESET_PRELOAD_NOTEON', {
+			['ZYNTHIAN_SCRIPT_MIDI_PROFILE', {
+				'type': 'select',
+				'title': 'MIDI profile',
+				'value': self.current_midi_profile_script,
+				'options': self.midi_profile_scripts,
+				'option_labels': {script_name: os.path.basename(script_name).split('.')[0] for script_name  in self.midi_profile_scripts},
+				'presets': self.midi_profile_presets
+			}],
+			['zynthian_midi_profile_delete_script', {
+				'type': 'button',
+				'title': 'Delete MIDI profile',
+				'button_type': 'submit',
+				'class': 'btn-danger',
+				'script_file': 'midi_profile_delete.js'
+			}],
+			['zynthian_midi_profile_new_script_name', {
+				'type': 'text',
+				'title': 'New MIDI profile',
+				'value': ''
+			}],
+			['zynthian_midi_profile_new_script', {
+				'type': 'button',
+				'title': 'New MIDI profile',
+				'button_type': 'button',
+				'class': 'btn-success',
+				'script_file': 'midi_profile_new.js'
+			}],
+			['ZYNTHIAN_MIDI_PRESET_PRELOAD_NOTEON', {
 				'type': 'boolean',
 				'title': 'Preset preload on Note-On',
-				'value': os.getenv('ZYNTHIAN_PRESET_PRELOAD_NOTEON',"0")
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_PRESET_PRELOAD_NOTEON')
 			}],
 			['ZYNTHIAN_MIDI_FINE_TUNING', {
 				'type': 'select',
 				'title': 'MIDI fine tuning (Hz)',
-				'value':  os.getenv('ZYNTHIAN_MIDI_FINE_TUNING','440'),
+				'value':  self.get_midi_env('ZYNTHIAN_MIDI_FINE_TUNING'),
 				'options': map(lambda x: str(x).zfill(2), list(range(392, 493)))
 			}],
-			['ZYNTHIAN_MASTER_MIDI_CHANNEL', {
+			['ZYNTHIAN_MIDI_MASTER_CHANNEL', {
 				'type': 'select',
 				'title': 'Master MIDI channel',
-				'value': os.getenv('MIDI_MASTER_CHANNEL','16'),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_MASTER_CHANNEL'),
 				'options': map(lambda x: str(x).zfill(2), list(range(1, 17)))
 			}],
-			['ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_CCNUM', {
+			['ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_CCNUM', {
 				'type': 'select',
 				'title': 'Master Bank change mode',
-				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_CCNUM',''),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_CCNUM'),
 				'options': list(self.program_change_mode_labels.keys()),
 				'option_labels': self.program_change_mode_labels,
 				'advanced': True
 			}],
-			['ZYNTHIAN_MASTER_MIDI_CHANGE_TYPE', {
+			['ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_TYPE', {
 				'type': 'select',
 				'title': 'Master change type',
-				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_CHANGE_TYPE',''),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_TYPE'),
 				'options': list(self.midi_program_change_presets.keys()),
 				'presets': self.midi_program_change_presets
 			}],
-			['ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_UP', {
+			['ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_UP', {
 				'type': 'text',
 				'title': 'Master Program change-up',
-				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_UP',''),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_UP'),
 				'advanced': True
 			}],
-			['ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_DOWN', {
+			['ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_DOWN', {
 				'type': 'text',
 				'title': 'Master Program change-down',
-				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_PROGRAM_CHANGE_DOWN',''),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_MASTER_PROGRAM_CHANGE_DOWN'),
 				'advanced': True
 			}],
-			['ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_UP', {
+			['ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_UP', {
 				'type': 'text',
 				'title': 'Master Bank change-up',
-				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_UP',''),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_UP'),
 				'advanced': True
 			}],
-			['ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_DOWN', {
+			['ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_DOWN', {
 				'type': 'text',
 				'title': 'Master Bank change-down',
-				'value': os.getenv('ZYNTHIAN_MASTER_MIDI_BANK_CHANGE_DOWN',''),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_MASTER_BANK_CHANGE_DOWN'),
 				'advanced': True
+			}],
+			['ZYNTHIAN_MIDI_NETWORK_ENABLED', {
+				'type': 'boolean',
+				'title': 'MIDI network enabled',
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_NETWORK_ENABLED')
 			}],
 			['ZYNTHIAN_MIDI_FILTER_RULES', {
 				'type': 'textarea',
 				'title': 'Midi filter rules',
-				'value': os.getenv('ZYNTHIAN_MIDI_FILTER_RULES',""),
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_FILTER_RULES'),
 				'cols': 50,
 				'rows': 5,
 				'addButton': 'display_midi_filter_rule_panel',
@@ -216,7 +270,7 @@ class MidiConfigHandler(ZynthianConfigHandler):
 			['ZYNTHIAN_MIDI_PORTS', {
 				'type': 'textarea',
 				'title': 'MIDI Ports',
-				'value': os.getenv('ZYNTHIAN_MIDI_PORTS',''),
+				'value':self.get_midi_env('ZYNTHIAN_MIDI_PORTS'),
 				'cols': 50,
 				'rows': 2,
 				'addButton': 'display_midi_ports_panel',
@@ -232,22 +286,52 @@ class MidiConfigHandler(ZynthianConfigHandler):
 			self.render("config.html", body="config_block.html", config=config, title="MIDI", errors=errors)
 
 	def post(self):
-		self.request.arguments['ZYNTHIAN_PRESET_PRELOAD_NOTEON'] = self.request.arguments.get('ZYNTHIAN_PRESET_PRELOAD_NOTEON','0')
+		self.request.arguments['ZYNTHIAN_MIDI_PRESET_PRELOAD_NOTEON'] = self.request.arguments.get('ZYNTHIAN_MIDI_PRESET_PRELOAD_NOTEON','0')
+		self.request.arguments['ZYNTHIAN_MIDI_NETWORK_ENABLED'] = self.request.arguments.get('ZYNTHIAN_MIDI_NETWORK_ENABLED','0')
+
+
+
 		escaped_request_arguments = tornado.escape.recursive_unicode(self.request.arguments)
 
-		filterError = self.validate_filter_rules(escaped_request_arguments);
-
-		if not filterError:
+		filter_error = self.validate_filter_rules(escaped_request_arguments);
+		errors = {}
+		if not filter_error:
 			# remove fields that sttart with FILTER_ADD from request_args, so that they won't be passed to update_config
 			for filter_add_argument in list(escaped_request_arguments.keys()):
 				if filter_add_argument.startswith('FILTER_ADD'):
 					del escaped_request_arguments[filter_add_argument]
 
-			errors = self.update_config(escaped_request_arguments)
+			new_profile_script_name = self.get_argument('zynthian_midi_profile_new_script_name')
+
+			if new_profile_script_name:
+				#New MIDI profile
+				new_profile_script_path = self.PROFILE_USER_DIRECTORY + '/' + new_profile_script_name + '.sh'
+				self.update_profile(new_profile_script_path, escaped_request_arguments)
+				mode = os.stat(new_profile_script_path).st_mode
+				mode |= (mode & 0o444) >> 2	# copy R bits to X
+				os.chmod(new_profile_script_path, mode)
+				self.load_midi_profile_directories()
+			elif 'zynthian_midi_profile_delete_script' in self.request.arguments and self.get_argument('zynthian_midi_profile_delete_script') == "1":
+				#DELETE
+				if self.current_midi_profile_script.startswith(self.PROFILE_USER_DIRECTORY):
+					os.remove(self.current_midi_profile_script)
+				else:
+					errors['zynthian_midi_profile_delete_script'] = 'You can only delete user profiles!'
+			else:
+				#SAVE
+				self.update_profile(self.current_midi_profile_script, escaped_request_arguments)
+				errors = self.update_config(escaped_request_arguments)
 			self.restart_ui()
 		else:
-			errors = {'ZYNTHIAN_MIDI_FILTER_RULES':filterError};
+			errors = {'ZYNTHIAN_MIDI_FILTER_RULES':filter_error};
 		self.get(errors)
+
+	def load_midi_profile_directories(self):
+		self.midi_profile_scripts = [self.PROFILE_SYS_DIRECTORY + '/' + x for x in os.listdir(self.PROFILE_SYS_DIRECTORY)]
+		self.midi_profile_scripts.extend([self.PROFILE_USER_DIRECTORY + '/' + x for x in os.listdir(self.PROFILE_USER_DIRECTORY)])
+		self.current_midi_profile_script = os.getenv('ZYNTHIAN_SCRIPT_MIDI_PROFILE',self.midi_profile_scripts[0])
+		if 'ZYNTHIAN_SCRIPT_MIDI_PROFILE' in self.request.arguments:
+			self.current_midi_profile_script = self.get_argument('ZYNTHIAN_SCRIPT_MIDI_PROFILE')
 
 	def validate_filter_rules(self, escaped_request_arguments):
 		if escaped_request_arguments['ZYNTHIAN_MIDI_FILTER_RULES'][0]:
@@ -258,24 +342,72 @@ class MidiConfigHandler(ZynthianConfigHandler):
 				return "ERROR parsing MIDI filter rule: " + str(e)
 
 	def get_ports_config(self):
-		client = jack.Client("ZynthianWebConf")
-		midi_in_ports = client.get_ports( is_midi=True, is_physical=True, is_input=True)
-		midi_out_ports = client.get_ports( is_midi=True, is_physical=True, is_output=True)
-		#midi_in_ports = client.get_ports( is_physical=True, is_output=True)
-		#midi_out_ports = client.get_ports( is_physical=True, is_input=True)
-
-		current_midi_ports = os.getenv('ZYNTHIAN_MIDI_PORTS','')
-		logging.info(current_midi_ports)
 		midi_ports = []
-		for idx,midi_port in enumerate(midi_in_ports):
-			midi_ports.append({
-				'midi_in':midi_port.name,
-				'midi_out':midi_out_ports[idx].name,
-				'checked_in': 'checked="checked"' if midi_port.name in current_midi_ports else '',
-				'checked_out': 'checked="checked"' if midi_out_ports[idx].name in current_midi_ports else ''})
+		try:
+			client = jack.Client("ZynthianWebConf")
+			midi_in_ports = client.get_ports( is_midi=True, is_physical=True, is_input=True)
+			midi_out_ports = client.get_ports( is_midi=True, is_physical=True, is_output=True)
+			#midi_in_ports = client.get_ports( is_physical=True, is_output=True)
+			#midi_out_ports = client.get_ports( is_physical=True, is_input=True)
+
+			current_midi_ports = os.getenv('ZYNTHIAN_MIDI_PORTS','')
+			#logging.info(current_midi_ports)
+
+			for idx,midi_port in enumerate(midi_in_ports):
+				midi_ports.append({
+					'midi_in':midi_port.shortname,
+					'midi_out':midi_out_ports[idx].shortname,
+					'checked_in': 'checked="checked"' if midi_port.shortname in current_midi_ports else '',
+					'checked_out': 'checked="checked"' if midi_out_ports[idx].shortname in current_midi_ports else ''})
+
+		except Exception as e:
+			logging.error(e)
+
 		ports_config=OrderedDict([
 			['MIDI_PORTS', midi_ports]
 		])
-
-		#logging.info(str(ports_config))
 		return ports_config
+		#logging.info(str(ports_config))
+
+
+	def load_midi_profiles(self):
+
+		p = re.compile("export (\w*)=\"(.*)\"")
+		invalidFiles = []
+		for midi_profile_script in self.midi_profile_scripts:
+			#logging.info(midi_profile_script)
+			profile_values = {}
+			try:
+				with open(midi_profile_script) as f:
+					for line in f:
+						m = p.match(line)
+						if m:
+							profile_values[m.group(1)] = m.group(2)
+				self.midi_profile_presets[midi_profile_script] = profile_values
+			except:
+				invalidFiles.append(midi_profile_script)
+
+		for midi_profile_script in invalidFiles:
+			logging.info("invalid file will be ignored: " + midi_profile_script)
+			self.midi_profile_scripts.remove(midi_profile_script)
+
+
+	def get_midi_env(self, key, default=''):
+		if key in self.midi_envs:
+			return self.midi_envs[key]
+		else:
+			return default
+
+
+
+	def update_profile(self, script_file_name, request_arguments):
+		with open(script_file_name, 'w+') as f:
+			f.write('#!/bin/bash\n')
+			midiEntries = []
+			for parameter in request_arguments:
+				if parameter.startswith('ZYNTHIAN_MIDI'):
+					f.write('export %s="%s"\n' % (parameter, request_arguments[parameter][0]))
+					midiEntries.append(parameter)
+
+			for k in midiEntries:
+				del request_arguments[k]
