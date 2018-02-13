f0dd46d258771ddb1657d48aff96caf9b21a940f
Holger Wirtz <holger@zynthian.org>
2018-02-13 14:55:59 +0000

    Now you can upload the originial pianoteq file and this will be installed for the pianoteq-engine. Changed uploading to only accepting 7z files.

--- lib/pianoteq_handler.py
+++ lib/pianoteq_handler.py
@@ -27,7 +27,9 @@ import logging
 import tornado.web
 import json
 import requests
-import tarfile
+import subprocess
+import shlex
+import shutil
 
 from collections import OrderedDict
 
@@ -73,9 +75,36 @@ class PianoteqHandler(tornado.web.RequestHandler):
 
 	def do_install_pianoteq(self):
 		filename = self.get_argument('ZYNTHIAN_PIANOTEQ_FILENAME');
-		logging.info(filename)
-		tar = tarfile.open(filename, "r:gz")
-		for member in tar.getmembers():
-			logging.info(member)
-			tar.extract(member, "/")
-		tar.close()
+		logging.info("Installing %s" % filename)
+
+		# Just to be sure...
+		if(os.path.isdir("/tmp/pianoteq")):
+			shutil.rmtree("/tmp/pianoteq")
+		if(not os.path.isdir("/zynthian/zynthian-sw/pianoteq6")):
+			os.mkdir("/zynthian/zynthian-sw/pianoteq6")
+
+		# Install binary
+		subprocess.call(shlex.split("/usr/bin/7z x -o/tmp/pianoteq %s \"Pianoteq 6 STAGE/arm/Pianoteq 6 STAGE*\"" % filename))
+		if(os.path.isfile("/zynthian/zynthian-sw/pianoteq6/Pianoteq 6 STAGE")):
+			logging.info("Removing old pianoteq binary")
+			os.remove("/zynthian/zynthian-sw/pianoteq6/Pianoteq 6 STAGE")
+		logging.info("Installing new pianoteq binary")
+		shutil.move("/tmp/pianoteq/Pianoteq 6 STAGE/arm/Pianoteq 6 STAGE","/zynthian/zynthian-sw/pianoteq6/")
+
+		# Install LV2 plugin
+		if(os.path.isdir("/zynthian/zynthian-sw/pianoteq6/Pianoteq 6 STAGE.lv2")):
+			logging.info("Removing old pianoteq LV2 plugin")
+			shutil.rmtree("/zynthian/zynthian-sw/pianoteq6/Pianoteq 6 STAGE.lv2")
+		logging.info("Installing new pianoteq LV2 plugin")
+		shutil.move("/tmp/pianoteq/Pianoteq 6 STAGE/arm/Pianoteq 6 STAGE.lv2","/zynthian/zynthian-sw/pianoteq6/")
+		if(os.path.islink("/zynthian/zynthian-plugins/lv2/Pianoteq 6 STAGE.lv2")):
+			os.unlink("/zynthian/zynthian-plugins/lv2/Pianoteq 6 STAGE.lv2")
+		elif(os.path.isdir("/zynthian/zynthian-plugins/lv2/Pianoteq 6 STAGE.lv2")):
+			shutil.rmtree("/zynthian/zynthian-plugins/lv2/Pianoteq 6 STAGE.lv2")
+		else:
+			os.remove("/zynthian/zynthian-plugins/lv2/Pianoteq 6 STAGE.lv2")
+		os.symlink("/zynthian/zynthian-sw/pianoteq6/Pianoteq 6 STAGE.lv2","/zynthian/zynthian-plugins/lv2/Pianoteq 6 STAGE.lv2")
+
+		# Cover my tracks
+		if(os.path.isdir("/tmp/pianoteq")):
+			shutil.rmtree("/tmp/pianoteq")
