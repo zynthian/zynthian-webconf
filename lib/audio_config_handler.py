98dae7679a1a70e5eccef2b437924877387a68c9
Markus Heidt <markus@heidt-tech.com>
2018-03-17 08:57:17 +0000

    Audioinjector related ALSA_Volume bug on save

--- lib/audio_config_handler.py
+++ lib/audio_config_handler.py
@@ -204,7 +204,7 @@ class AudioConfigHandler(ZynthianConfigHandler):
 					if m:
 						volumePercent = m.group(2)
 						channelType = m.group(1)
-			if controlName and playbackChannel:
+			if controlName and channelTpe:
 				self.add_mixer_control(config, mixerControl, controlName, volumePercent, channelType)
 		except Exception as err:
 			logging.error(format(err))
