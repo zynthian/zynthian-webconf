78c4ac38f6d34cbe81f381606c3a97ae9359eec1
jofemodo <fernando@zynthian.org>
2019-06-21 13:42:32 +0200

    Use branches instead of tags as it better fit the current development workflow.

--- lib/repository_handler.py
+++ lib/repository_handler.py
@@ -40,29 +40,29 @@ from lib.wiring_config_handler import WiringConfigHandler
 
 class RepositoryHandler(ZynthianConfigHandler):
 
+	zynthian_base_dir = os.environ.get('ZYNTHIAN_DIR', "/zynthian")
+
 	repository_list = [
-		'zyncoder',
-		'zynthian-ui',
-		'zynthian-sys',
-		'zynthian-data',
-		'zynthian-webconf'
+		['zynthian-ui', False],
+		['zynthian-webconf', False],
+		['zynthian-sys', True],
+		['zynthian-data', True],
+		['zyncoder', True]
 	]
 
 	@tornado.web.authenticated
 	def get(self, errors=None):
-
-
-
 		config = OrderedDict([])
 
-		for repository_name in self.repository_list:
-			tag_list = self.get_tag_list(repository_name)
-			config['ZYNTHIAN_REPO_%s' % repository_name] = {
+		for repitem in self.repository_list:
+			tag_list = self.get_repo_branch_list(repitem[0])
+			config["ZYNTHIAN_REPO_{}".format(repitem[0])] = {
 				'type': 'select',
-				'title': repository_name,
-				'value': self.get_current_branch(repository_name),
+				'title': repitem[0],
+				'value': self.get_repo_current_branch(repitem[0]),
 				'options': tag_list,
-				'option_labels': OrderedDict([(tag_name, tag_name) for tag_name in tag_list])
+				'option_labels': OrderedDict([(tag_name, tag_name) for tag_name in tag_list]),
+				'advanced': repitem[1]
 			}
 
 		super().get("Repositories", config, errors)
@@ -71,36 +71,73 @@ class RepositoryHandler(ZynthianConfigHandler):
 	@tornado.web.authenticated
 	def post(self):
 		postedConfig = tornado.escape.recursive_unicode(self.request.arguments)
-
 		logging.info(postedConfig)
-		for posted_config_key in postedConfig:
-			repository_name = posted_config_key[14:]
-			logging.info(repository_name)
-			self.update_branch(repository_name, postedConfig[posted_config_key][0])
 
 		errors = {}
+		for posted_config_key in postedConfig:
+			repo_name = posted_config_key[14:]
+			logging.info(repo_name)
+			try:
+				self.set_repo_branch(repo_name, postedConfig[posted_config_key][0])
+			except Exception as err:
+				logging.error(err)
+				errors[repo_name]=err
+
 		self.get(errors)
 
-		return errors
 
-	def get_tag_list(self, repository_name):
+	def get_repo_tag_list(self, repo_name):
 		result = []
 		result.append('master')
+		repo_dir = self.zynthian_base_dir + "/" + repo_name
 
-		for byteLine in check_output("cd /zynthian/%s; git tag" % repository_name, shell=True).splitlines():
+		check_output("cd {}; git remote update origin --prune".format(repo_dir), shell=True)
+		for byteLine in check_output("cd {}; git tag".format(repo_dir), shell=True).splitlines():
 			result.append(byteLine.decode("utf-8"))
 
 		return result
 
-	def get_current_branch(self, repository_name):
-		for byteLine in check_output("cd /zynthian/%s; git branch | grep \* | cut -d ' ' -f2" % repository_name, shell=True).splitlines():
+
+	def get_repo_branch_list(self, repo_name):
+		result = []
+		repo_dir = self.zynthian_base_dir + "/" + repo_name
+
+		check_output("cd {}; git remote update origin --prune".format(repo_dir), shell=True)
+		for byteLine in check_output("cd {}; git branch -a".format(repo_dir), shell=True).splitlines():
+			branch_name=byteLine.decode("utf-8")
+			if branch_name.startswith("*"):
+				branch_name = branch_name[2:]
+			if "->" in branch_name:
+				continue
+			result.append(branch_name)
+
+		return result
+
+
+	def get_repo_current_branch(self, repo_name):
+		repo_dir = self.zynthian_base_dir + "/" + repo_name
+
+		for byteLine in check_output("cd {}; git branch | grep \* | cut -d ' ' -f2".format(repo_dir), shell=True).splitlines():
 			return byteLine.decode("utf-8")
 
-	def update_branch(self, repository_name, new_branch_name):
-		current_branch = self.get_current_branch(repository_name)
-		if new_branch_name != current_branch:
-			logging.info("needs change %s != %s" % (current_branch, new_branch_name))
-			if new_branch_name == 'master':
-				check_output("cd /zynthian/%s; git checkout %s" % (repository_name, new_branch_name), shell=True)
+
+	def set_repo_tag(self, repo_name, tag_name):
+		repo_dir = self.zynthian_base_dir + "/" + repo_name
+		current_branch = self.get_repo_current_branch(repo_name)
+
+		if tag_name != current_branch:
+			logging.info("needs change {}!= {}".format(current_branch, tag_name))
+			if tag_name == 'master':
+				check_output("cd {}; git checkout .; git checkout {}".format(repo_dir, tag_name), shell=True)
 			else:
-				check_output("cd /zynthian/%s; git branch -d %s;git checkout tags/%s -b %s" % (repository_name, new_branch_name, new_branch_name, new_branch_name), shell=True)
+				check_output("cd {}; git checkout .; git branch -d {}; git checkout tags/{} -b {}".format(repo_dir, tag_name, tag_name, tag_name), shell=True)
+
+
+	def set_repo_branch(self, repo_name, branch_name):
+		repo_dir = self.zynthian_base_dir + "/" + repo_name
+		current_branch = self.get_repo_current_branch(repo_name)
+
+		if branch_name != current_branch:
+			logging.info("needs change {}!= {}".format(current_branch, branch_name))
+			check_output("cd {}; git checkout .; git checkout {}".format(repo_dir, branch_name), shell=True)
+
