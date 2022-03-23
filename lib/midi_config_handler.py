eaef80e159a57430d77afaa64b37c91afc66255f
jofemodo <fernando@zynthian.org>
2022-03-23 19:39:14 +0100

    Add MIDI config option for enabling/disabling Bank Change with CC0 & CC32.

--- lib/midi_config_handler.py
+++ lib/midi_config_handler.py
@@ -410,8 +410,14 @@ class MidiConfigHandler(ZynthianConfigHandler):
 			}],
 			['ZYNTHIAN_MIDI_PROG_CHANGE_ZS3', {
 				'type': 'boolean',
-				'title': 'ZS3 (Capture Program Change events for SubSnapShots)',
+				'title': 'Program Change ZS3 (Program Change for SubSnapShots)',
 				'value': self.get_midi_env('ZYNTHIAN_MIDI_PROG_CHANGE_ZS3','1'),
+				'advanced': False
+			}],
+			['ZYNTHIAN_MIDI_BANK_CHANGE', {
+				'type': 'boolean',
+				'title': 'Bank Change with CC#0(MSB) & CC#32(LSB)',
+				'value': self.get_midi_env('ZYNTHIAN_MIDI_BANK_CHANGE','0'),
 				'advanced': False
 			}],
 			['ZYNTHIAN_MIDI_PRESET_PRELOAD_NOTEON', {
@@ -551,6 +557,7 @@ class MidiConfigHandler(ZynthianConfigHandler):
 		self.request.arguments['ZYNTHIAN_MIDI_PRESET_PRELOAD_NOTEON'] = self.request.arguments.get('ZYNTHIAN_MIDI_PRESET_PRELOAD_NOTEON','0')
 		self.request.arguments['ZYNTHIAN_MIDI_SINGLE_ACTIVE_CHANNEL'] = self.request.arguments.get('ZYNTHIAN_MIDI_SINGLE_ACTIVE_CHANNEL','0')
 		self.request.arguments['ZYNTHIAN_MIDI_PROG_CHANGE_ZS3'] = self.request.arguments.get('ZYNTHIAN_MIDI_PROG_CHANGE_ZS3','0')
+		self.request.arguments['ZYNTHIAN_MIDI_BANK_CHANGE'] = self.request.arguments.get('ZYNTHIAN_MIDI_BANK_CHANGE','0')
 		self.request.arguments['ZYNTHIAN_MIDI_RTPMIDI_ENABLED'] = self.request.arguments.get('ZYNTHIAN_MIDI_RTPMIDI_ENABLED','0')
 		self.request.arguments['ZYNTHIAN_MIDI_NETWORK_ENABLED'] = self.request.arguments.get('ZYNTHIAN_MIDI_NETWORK_ENABLED','0')
 		self.request.arguments['ZYNTHIAN_MIDI_TOUCHOSC_ENABLED'] = self.request.arguments.get('ZYNTHIAN_MIDI_TOUCHOSC_ENABLED','0')
