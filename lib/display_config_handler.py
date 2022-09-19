6d785f917ccb8e9dbf13bbf0e8ebbf877ce86897
jofemodo <fernando@zynthian.org>
2022-09-19 20:00:53 +0200

    + Add kernel options to display configuration (cmdline.txt) + Start using KMS kernel driver for Z2 display (vc4-kms-v3d).

--- lib/display_config_handler.py
+++ lib/display_config_handler.py
@@ -45,10 +45,12 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'hdmi_mode:1=87\n'+
 				'hdmi_cvt:1 1024 600 60 6 0 0 0\n'+
 				'#config_hdmi_boost:1=4\n'+
-				'display_rotate=2\n'+
+				'#display_rotate=2\n'+
+				'dtoverlay=vc4-kms-v3d\n'+
 				'dtoverlay=goodix_z2',
 			'DISPLAY_WIDTH': '1024',
 			'DISPLAY_HEIGHT': '600',
+			'DISPLAY_KERNEL_OPTIONS': 'video=HDMI-A-2:1024x600M@60,rotate=180',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['ZynScreen 3.5 (v1)', {
@@ -56,6 +58,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,speed=2000000,cs=1,penirq=17,penirq_pull=2,swapxy=1,xohms=100,pmax=255',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['PiScreen 3.5 (v2)', {
@@ -63,36 +66,42 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,speed=2000000,cs=1,penirq=17,penirq_pull=2,swapxy=1,xohms=100,pmax=255',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['PiScreen 3.5 (v1)', {
 			'DISPLAY_CONFIG': 'dtoverlay=piscreen,speed=16000000,rotate=90',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['PiTFT 2.8 Resistive', {
 			'DISPLAY_CONFIG': 'dtoverlay=pitft28-resistive,rotate=90,speed=32000000,fps=20',
 			'DISPLAY_WIDTH': '320',
 			'DISPLAY_HEIGHT': '240',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['PiTFT 2.8 Capacitive', {
 			'DISPLAY_CONFIG': 'dtoverlay=pitft28-capacitive,rotate=90,speed=32000000,fps=20',
 			'DISPLAY_WIDTH': '320',
 			'DISPLAY_HEIGHT': '240',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['PiTFT 3.5 Resistive', {
 			'DISPLAY_CONFIG': 'dtoverlay=pitft35-resistive,rotate=90,speed=32000000,fps=20',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['RPi-Display 2.8', {
 			'DISPLAY_CONFIG': 'dtoverlay=rpi-display,speed=32000000,rotate=270',
 			'DISPLAY_WIDTH': '320',
 			'DISPLAY_HEIGHT': '240',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 3.2B', {
@@ -101,6 +110,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=0,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '320',
 			'DISPLAY_HEIGHT': '240',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 3.2C', {
@@ -109,6 +119,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=0,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '320',
 			'DISPLAY_HEIGHT': '240',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 3.5A', {
@@ -117,6 +128,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=1,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 3.5B', {
@@ -125,6 +137,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=1,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 3.5B V2', {
@@ -133,6 +146,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=1,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 3.5C', {
@@ -141,6 +155,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=1,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 4A GPIO-only', {
@@ -149,6 +164,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=1,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 4c GPIO-only', {
@@ -157,6 +173,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'#dtoverlay=ads7846,cs=1,penirq=17,penirq_pull=2,speed=1000000,keep_vref_on=1,swapxy=1,pmax=255,xohms=60,xmin=200,xmax=3900,ymin=200,ymax=3900\n',
 			'DISPLAY_WIDTH': '480',
 			'DISPLAY_HEIGHT': '320',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['WaveShare 4 HDMI+GPIO', {
@@ -170,6 +187,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=983,xmax=63241,ymin=3015,ymax=62998\n',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['WaveShare 4.3 HDMI+GPIO', {
@@ -183,6 +201,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=200,xmax=3900,ymin=200,ymax=3900',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['WaveShare 5 HDMI+GPIO', {
@@ -196,6 +215,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=200,xmax=3900,ymin=200,ymax=3900',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['WaveShare 5 HDMI+USB', {
@@ -208,6 +228,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'hdmi_cvt 800 480 60 6 0 0 0',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['WaveShare 7 HDMI+GPIO 1024x600', {
@@ -221,6 +242,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=200,xmax=3900,ymin=200,ymax=3900',
 			'DISPLAY_WIDTH': '1024',
 			'DISPLAY_HEIGHT': '600',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['WaveShare 7 HDMI+GPIO 800x480', {
@@ -234,6 +256,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=200,xmax=3900,ymin=200,ymax=3900',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['WaveShare 7 HDMI+USB 1024x600', {
@@ -246,6 +269,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'hdmi_cvt 1024 600 60 6 0 0 0',
 			'DISPLAY_WIDTH': '1024',
 			'DISPLAY_HEIGHT': '600',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['WaveShare 7 HDMI+USB 800x480', {
@@ -258,12 +282,14 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'hdmi_cvt 800 480 60 6 0 0 0',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['Sainsmart 1.8', {
 			'DISPLAY_CONFIG': 'dtoverlay=sainsmart18,rotate=90',
 			'DISPLAY_WIDTH': '160',
 			'DISPLAY_HEIGHT': '128',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb1'
 		}],
 		['MHS35 480x320', {
@@ -289,6 +315,7 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=200,xmax=3900,ymin=200,ymax=3900',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['Pi 7 Touchscreen Display 800x480', {
@@ -297,18 +324,21 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'dtoverlay=rpi-ft5406\n',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['MIPI DSI 800x480', {
 			'DISPLAY_CONFIG': '',
 			'DISPLAY_WIDTH': '800',
 			'DISPLAY_HEIGHT': '480',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['Generic HDMI Display', {
 			'DISPLAY_CONFIG': 'disable_overscan=1\n',
 			'DISPLAY_WIDTH': '',
 			'DISPLAY_HEIGHT': '',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['Generic 4k HDMI Display', {
@@ -317,12 +347,14 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 				'hdmi_enable_4kp60=1\n',
 			'DISPLAY_WIDTH': '',
 			'DISPLAY_HEIGHT': '',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': '/dev/fb0'
 		}],
 		['Custom Device', {
 			'DISPLAY_CONFIG': '',
 			'DISPLAY_WIDTH': '',
 			'DISPLAY_HEIGHT': '',
+			'DISPLAY_KERNEL_OPTIONS': '',
 			'FRAMEBUFFER': ''
 		}]
 	])
@@ -371,6 +403,13 @@ class DisplayConfigHandler(ZynthianConfigHandler):
 			'type': 'text',
 			'title': 'Height',
 			'value': os.environ.get('DISPLAY_HEIGHT'),
+			'advanced': True,
+			'disabled': custom_options_disabled
+		}
+		config['DISPLAY_KERNEL_OPTIONS'] = {
+			'type': 'text',
+			'title': 'Kernel Options',
+			'value': os.environ.get('DISPLAY_KERNEL_OPTIONS'),
 			'advanced': True,
 			'disabled': custom_options_disabled
 		}
