0be46c9dc6d7af2d20cbe5e5c8c004f0f0735fb9
jofemodo <fernando@zynthian.org>
2022-12-01 20:20:09 +0100

    Modify webconf start script for working with new config file "zynthian_envars_extended.sh".

--- zynthian_webconf.sh
+++ zynthian_webconf.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ -f "./zynthian_envars.sh" ]; then
-	source "./zynthian_envars.sh"
-elif [ -d "$ZYNTHIAN_CONFIG_DIR" ]; then
-	source "$ZYNTHIAN_CONFIG_DIR/zynthian_envars.sh"
-else
-	source "$ZYNTHIAN_SYS_DIR/scripts/zynthian_envars.sh"
-fi
+source "$ZYNTHIAN_SYS_DIR/scripts/zynthian_envars_extended.sh"
 
 # 10=DEBUG, 20=INFO, 30=WARNING, 40=ERROR, 50=CRITICAL
 export ZYNTHIAN_LOG_LEVEL=10
