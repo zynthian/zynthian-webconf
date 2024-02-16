46971b2591d8b3cb04669bbed02ef5033d589619
jofemodo <fernando@zynthian.org>
2024-02-16 05:40:52 +0100

    Finish engine editor implementation. It saves every interaction using AJAX calls.

--- lib/engines_handler.py
+++ lib/engines_handler.py
@@ -22,6 +22,7 @@
 #
 # ********************************************************************
 
+import re
 import copy
 import json
 import logging
@@ -41,9 +42,13 @@ class EnginesHandler(ZynthianBasicHandler):
 	@tornado.web.authenticated
 	def get(self, errors=None):
 		config = {}
+
+		zynthian_lv2.load_engines()
+		#zynthian_lv2.sanitize_engines()
+
 		config['ZYNTHIAN_ENGINES'] = zynthian_lv2.engines_by_type
 
-		# Make a deep copy and remove not serializable objects (ENGINE)
+		# Make a deep copy AND remove not serializable objects (ENGINE)
 		sengines = copy.deepcopy(zynthian_lv2.engines)
 		for key, info in sengines.items():
 			try:
@@ -51,6 +56,7 @@ class EnginesHandler(ZynthianBasicHandler):
 			except:
 				pass
 		config['ZYNTHIAN_ENGINES_JSON'] = json.JSONEncoder().encode(sengines)
+		config['ZYNTHIAN_ENGINE_CATS_JSON'] = json.JSONEncoder().encode(zynthian_lv2.engine_categories)
 
 		try:
 			config['ZYNTHIAN_ACTIVE_TAB'] = self.get_argument('ZYNTHIAN_ACTIVE_TAB')
@@ -75,35 +81,41 @@ class EnginesHandler(ZynthianBasicHandler):
 
 	@tornado.web.authenticated
 	def post(self):
-		self.posted_args = tornado.escape.recursive_unicode(self.request.arguments)
-		action = self.posted_args['ZYNTHIAN_ENGINES_ACTION'][0]
+		action = self.get_argument('ZYNTHIAN_ENGINES_ACTION')
 		logging.debug(f"Executing {action} ...")
-		if action == 'ENABLE_ENGINES':
-			errors = self.do_enable_engines()
-		elif action == "REGENERATE_ENGINES":
-			errors = self.do_regenerate_engines()
+		if action == "REGENERATE_ENGINES":
+			errors = self.do_regenerate_engines(reset_rankings=0)
 		elif action == "REGENERATE_LV2_PRESETS_CACHE":
 			errors = self.do_regenerate_lv2_presets_cache()
 		else:
 			errors = {}
 		self.get(errors)
 
-	def do_enable_engines(self):
-		try:
-			for key, info in zynthian_lv2.engines.items():
-				if "ZYNTHIAN_ENGINES_ENABLE_{}".format(key) in self.posted_args:
-					info['ENABLED'] = True
-				else:
-					info['ENABLED'] = False
-			zynthian_lv2.save_engines()
-		except Exception as e:
-			logging.error("Enabling engines failed: {}".format(e))
-			return format(e)
+	@tornado.web.authenticated
+	def put(self):
+		ucargs = tornado.escape.recursive_unicode(self.request.arguments)
+		eng_code = ucargs['ENGINE_CODE'][0]
+		zynthian_lv2.engines[eng_code]['ENABLED'] = bool(int(ucargs['ENGINE_ENABLED'][0]))
+		zynthian_lv2.engines[eng_code]['NAME'] = ucargs['ENGINE_NAME'][0]
+		zynthian_lv2.engines[eng_code]['CAT'] = ucargs['ENGINE_CAT'][0]
+		zynthian_lv2.engines[eng_code]['QUALITY'] = int(ucargs['ENGINE_QUALITY'][0])
+		zynthian_lv2.engines[eng_code]['COMPLEX'] = int(ucargs['ENGINE_COMPLEX'][0])
+		zynthian_lv2.engines[eng_code]['DESCR'] = ucargs['ENGINE_DESCR'][0]
+		zynthian_lv2.save_engines()
 
-	def do_regenerate_engines(self):
+	@tornado.web.authenticated
+	def patch(self):
+		ucargs = tornado.escape.recursive_unicode(self.request.arguments)
+		eng_code = ucargs['ENGINE_CODE'][0]
+		eng_enabled = bool(int(ucargs['ENGINE_ENABLED'][0]))
+		zynthian_lv2.engines[eng_code]['ENABLED'] = eng_enabled
+		logging.debug(f"Engine '{eng_code}' => ENABLED={eng_enabled}")
+		zynthian_lv2.save_engines()
+
+	def do_regenerate_engines(self, reset_rankings=None):
 		prev_engines = zynthian_lv2.engines.keys()
 		# Regenerate engine info file, searching for LV2 plugins
-		zynthian_lv2.generate_engines_config_file()
+		zynthian_lv2.generate_engines_config_file(reset_rankings)
 		zynthian_lv2.get_engines_by_type()
 		# Detect new LV2 plugins and generate presets cache for them
 		for key, info in zynthian_lv2.engines.items():
