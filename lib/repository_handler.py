9941eebd8c65c5997bd43b8e88536a333d109902
jofemodo <fernando@zynthian.org>
2024-09-23 03:01:04 +0200

    Improve repository management to fit the new version paradigm: stable / testing / custom

--- lib/repository_handler.py
+++ lib/repository_handler.py
@@ -4,7 +4,7 @@
 #
 # Audio Configuration Handler
 #
-# Copyright (C) 2017 Fernando Moyano <jofemodo@zynthian.org>
+# Copyright (C) 2017-2024 Fernando Moyano <jofemodo@zynthian.org>
 #
 # ********************************************************************
 #
@@ -40,15 +40,17 @@ from lib.wiring_config_handler import WiringConfigHandler
 # ------------------------------------------------------------------------------
 
 class RepositoryHandler(ZynthianConfigHandler):
-	ZYNTHIAN_REPO_POST_VAR = "ZYNTHIAN_REPO_"
 	zynthian_base_dir = os.environ.get('ZYNTHIAN_DIR', "/zynthian")
+	stable_branch = os.environ.get('ZYNTHIAN_STABLE_BRANCH', "oram")
+	stable_tag = os.environ.get('ZYNTHIAN_STABLE_TAG', "2409")
+	testing_branch = os.environ.get('ZYNTHIAN_TESTING_BRANCH', "oram")
 
 	repository_list = [
-		['zynthian-ui', True],
-		['zynthian-webconf', True],
+		['zynthian-ui', False],
+		['zynthian-webconf', False],
+		['zyncoder', True],
 		['zynthian-sys', True],
-		['zynthian-data', True],
-		['zyncoder', True]
+		['zynthian-data', True]
 	]
 
 	@tornado.web.authenticated
@@ -59,30 +61,27 @@ class RepositoryHandler(ZynthianConfigHandler):
 	def post(self):
 		postedConfig = tornado.escape.recursive_unicode(self.request.arguments)
 		logging.info(postedConfig)
-
+		try:
+			version = postedConfig['ZYNTHIAN_VERSION'][0]
+		except:
+			version = self.stable_branch
 		errors = {}
 		changed_repos = 0
-		try:
-			is_stable = (postedConfig['STABLE'][0] == '1')
-		except:
-			is_stable = False
-		logging.debug(F"is_stable: {is_stable}")
 		for posted_config_key in postedConfig:
-			if posted_config_key.startswith(self.ZYNTHIAN_REPO_POST_VAR):
-				repo_name = posted_config_key[len(self.ZYNTHIAN_REPO_POST_VAR):]
+			if posted_config_key.startswith("ZYNTHIAN_REPO_"):
+				repo_name = posted_config_key[len("ZYNTHIAN_REPO_"):]
 				try:
-					branch = postedConfig[posted_config_key][0]
-					if is_stable:
-						if self.set_repo_branch(repo_name, 'stable'):
-							changed_repos += 1
+					if version == "custom":
+						branch = postedConfig[posted_config_key][0]
 					else:
-						if self.set_repo_branch(repo_name, branch):
-							changed_repos += 1
+						branch = version
+					if self.set_repo_branch(repo_name, branch):
+						changed_repos += 1
 				except Exception as err:
 					logging.error(err)
-					errors[F"{self.ZYNTHIAN_REPO_POST_VAR}{repo_name}"] = err
+					errors[f"ZYNTHIAN_REPO_{repo_name}"] = err
 
-		config = self.get_config_info()
+		config = self.get_config_info(version)
 		if changed_repos > 0:
 			config['ZYNTHIAN_MESSAGE'] = {
 				'type': 'html',
@@ -92,28 +91,49 @@ class RepositoryHandler(ZynthianConfigHandler):
 
 		super().get("Repositories", config, errors)
 
-	def get_config_info(self):
-		config = OrderedDict([])
-		config["STABLE"] = {
-			'type': 'boolean',
-			'title': 'Stable',
-			'value': '0',
-			'advanced': False
-		}
+	def get_config_info(self, version=None):
 		stable_overall = True
+		testing_overall = True
+		repo_branches = []
 		for repitem in self.repository_list:
-			options = self.get_repo_branch_list(repitem[0])
 			branch = self.get_repo_current_branch(repitem[0])
-			config[F"{self.ZYNTHIAN_REPO_POST_VAR}{repitem[0]}"] = {
+			stable_overall &= (branch == self.stable_branch)
+			testing_overall &= (branch == self.testing_branch)
+			repo_branches.append(branch)
+		if version is None:
+			if stable_overall:
+				version = self.stable_branch
+			elif testing_overall:
+				version = self.testing_branch
+			else:
+				version = "custom"
+
+		config = {
+			"ZYNTHIAN_VERSION": {
 				'type': 'select',
-				'title': repitem[0],
-				'value': branch,
-				'options': options,
-				'option_labels': OrderedDict([(opt, opt) for opt in options]),
-				'advanced': repitem[1]
+				'title': 'Version',
+				'value': version,
+				'options': [self.stable_branch, self.testing_branch, "custom"],
+				'option_labels': {self.stable_branch: f"stable ({self.stable_branch})", self.testing_branch: f"testing ({self.testing_branch})", "custom": "custom"},
+				'refresh_on_change': True,
+				'advanced': False
 			}
-			stable_overall &= (branch == 'stable')
-		config["STABLE"]['value'] = '1' if stable_overall else '0'
+		}
+		if version == "custom":
+			for i, repitem in enumerate(self.repository_list):
+				options = self.get_repo_branch_list(repitem[0])
+				config[f"ZYNTHIAN_REPO_{repitem[0]}"] = {
+					'type': 'select',
+					'title': repitem[0],
+					'value': repo_branches[i],
+					'options': options,
+					'option_labels': OrderedDict([(opt, opt) for opt in options]),
+					'advanced': repitem[1]
+				}
+		config['_SPACER_'] = {
+			'type': 'html',
+			'content': "<br>"
+		}
 		return config
 
 	def get_repo_tag_list(self, repo_name):
